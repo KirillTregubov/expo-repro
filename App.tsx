58a47a4766cddb9cfd37144cea19c81147f9264a
Kirill Tregubov <contact@kirilltregubov.com>
2025-02-28 00:51:54 -0500

    image repro

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Image } from 'expo-image';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+
+      <Image source="lines" style={{ width: 400, height: 80 }} />
+      <Image source={require('./assets/lines.png')} style={{ width: 400, height: 80 }} />
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
